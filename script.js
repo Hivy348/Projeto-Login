03fd3de30cb0e020c5e2162087b887eb97020ac8
Hivy348 <hivynnaapsmendes@gmail.com>
2026-02-03 18:38:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function mostrarCadastro() {
+    
+    document.getElementById("Login").style.display = "none";
+    document.getElementById("Cadastro").style.display = "block";
+}
+function mostrarLogin() {
+
+    document.getElementById("Cadastro").style.display = "none";
+    document.getElementById("Login").style.display = "block";
+}
